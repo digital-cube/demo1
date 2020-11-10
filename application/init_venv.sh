8707b2d072c448de4ae92dcf5113f02de4dec3e5
Igor Jeremic <igor@digitalcube.rs>
2020-11-10 21:02:14 +0100

    fix

--- application/init_venv.sh
+++
@@ -1,5 +0,0 @@
-rm -rf .venv
-python3 -m venv .venv
-.venv/bin/pip install --upgrade pip
-.venv/bin/pip install wheel
-.venv/bin/pip install -r requirements.txt
--- application/service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./wait4db.py && cd db_migrations && alembic upgrade head && cd - &&  ./service.py
